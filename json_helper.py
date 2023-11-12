d534d49cb1437b9ef39fa524b74094d0531d96a5
Corinne <CorinneKogut@gmail.com>
2023-11-12 16:47:23 -0500

    Finished the 4 exercises

--- json_helper.py
+++ json_helper.py
@@ -1,13 +1,12 @@
 import json
 import os
+import pickle
 
 
 # reads a json file and returns a dictionary
 def read_json(file_name):
     with open(file_name) as f:
-        x = json.load(f)
-
-    return x
+        return json.load(f)
 
 
 # takes a dir name and returns a list of dictionaries of the json files
@@ -22,14 +21,25 @@ def read_all_json_files(dir_name):
     return my_list
 
 
-def write_pickle():
+def write_pickle(obj, output):
+    with open(output, 'wb') as f:
+        pickle.dump(obj, f)
 
 
+def load_pickle(file_name):
+    f = open(file_name, "rb")
+    return pickle.load(f)
 
-# y = read_json('data/super_smash_bros/link.json')
-# print(y)
-# print(type(y))
+
+y = read_json('data/super_smash_bros/link.json')
+print(y)
+print(type(y))
 
 z = read_all_json_files('data/super_smash_bros')
 print(z)
 print(type(z))
+
+write_pickle(y, 'super_smash_characters.pickle')
+print(load_pickle('super_smash_characters.pickle'))
+
+
